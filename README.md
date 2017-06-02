638a4715489db220554e09a08ff5ef360889e67b
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-06-02 09:26:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Leveler
+
+Leveler is a simple little bit of Jquery code that helps set matching height on elements.
+
+---
+
+## Dependencies
+
+* Jquery - Reference it in the document.  Any path could be used so if the jQuery files are local, or you need a different version, just adjust the path to fit.
+````shell
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+````
+
+## Setup
+
+To use leveler just copy the `levler.js` file into your javascript files directory.  Make sure you reference it after JQuery.
+
+## Usage
+
+Using leveler is easy.  Add the class `level` to the elements you want to level out.
---
+++ level.js
@@ -0,0 +1,40 @@
+$(function () {
+  
+  // For Ruby on Rails applications.  Feel free to comment this out or removie it.
+  $(document).on('turbolinks:load', function() {
+    // Find all the elements to level
+    var el = document.getElementsByClassName('level');
+    // If there are none we do not need to do anything
+    if (el.length > 0 ) {
+      // Set the default value
+      var maxHeight = -1;
+      // Iterate through each element found
+      $('.level').each(function(){
+        // Get the highest value 
+        maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+        // console.log(maxHeight);
+      });
+      // Level the elements out to the highest value
+      $('.level').css('height', maxHeight);
+    }
+  });
+
+  // 
+  $(document).on('load', function() {
+    // Find all the elements to level
+    var el = document.getElementsByClassName("level");
+    // If there are none we do not need to do anything
+    if (el.length > 0 ) {
+      // Set the default value
+      var maxHeight = -1;
+      // Iterate through each element found
+      $('.level').each(function(){
+        // Get the highest value 
+        maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+        // console.log(maxHeight);
+      });
+      // Level the elements out to the highest value
+      $('.level').css('height', maxHeight);
+    }
+  });
+});
